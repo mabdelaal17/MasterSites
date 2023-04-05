5687c71ca2422f66e7e49fa3e5d68d70abffc4ea
MahmoudAbdelaal7 <125163199+MahmoudAbdelaal7@users.noreply.github.com>
2023-04-05 17:35:46 +0200

    Fixing bug

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ resetButton.addEventListener("click", () => {
   localStorage.setItem("page_view", 1);
   counterContainer.innerHTML = visitCount;
 });
+
+// Get all elements with class name "section-header"
+var elements = document.getElementsByClassName("section-header");
+
+// Get the count of elements
+var count = elements.length;
+
+// Get the existing section header element with class name "section-header"
+var sectionHeader = document.querySelector(".section-header");
+
+// Get the existing p element with class name "secondary-sections-counter"
+var counter = sectionHeader.querySelector(".secondary-sections-counter");
+
+// Update the text content of the p element with the count of elements
+counter.textContent = count;
