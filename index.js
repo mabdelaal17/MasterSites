40f837eadeb60d65d55565947388ac5e77f89c23
mAbdelaal7 <anaelmalik206@yahoo.com>
2023-04-06 19:31:32 +0200

    Update

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ console.log(clas.length);
 document.getElementsByClassName("secondary-section-counter");
 document.getElementById("secondary-section-counter").innerHTML = clas.length;
 
-// var clas = document.getElementsByClassName("site-container");
-// console.log(clas.length);
-// document.getElementsByClassName("site-counter");
-// document.getElementById("site-counter").innerHTML = clas.length;
-
 const urls = ["./index.html", "./everyone/icons-more/icons-more.html"];
 var nbOccurences = 0;
-for (let n = 0; n < urls.length - 1; n++) {
-  console.log(urls[n]);
+
+for (let n = 0; n < urls.length; n++) {
   fetch(urls[n], { cache: "no-store" }).then((response) => {
-    response.text().then((data) => {
-      // (data) is the whole source-code inside your html
-      var test = "site-container";
-      nbOccurences += data.split(test).length - 1;
-    });
+    response
+      .text()
+      .then((data) => {
+        nbOccurences += Number(
+          (data.match(new RegExp("site-container", "g")) || []).length
+        );
+      })
+      .then((data) => {
+        console.log("nb Occurences : ", nbOccurences);
+        document.querySelector("h2#site-counter").innerText = nbOccurences;
+      });
   });
 }
-document.querySelector("h2#site-counter").innerText = nbOccurences;
