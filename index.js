8ac58dcbf2622ae8329ecf16769af290b1fc68f9
mAbdelaal7 <anaelmalik206@yahoo.com>
2023-04-07 00:33:11 +0200

    updated

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
-var visitCount = localStorage.getItem("page_view");
-
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-}
-counterContainer.innerHTML = visitCount;
-
 // Main Sections Counter
 var clas = document.getElementsByClassName("section-header");
 console.log(clas.length);
