3cffa4a282f2312cb95d9ddf75ced1d10c3ca5d4
mAbdelaal7 <anaelmalik206@yahoo.com>
2023-04-07 14:58:11 +0200

    update

--- index.js
+++ index.js
@@ -36,3 +36,59 @@ let counter = 0;
 
   document.querySelector("h2#site-counter").innerText = counter;
 })();
+
+// Image Copression
+const MAX_WIDTH = 50;
+const MAX_HEIGHT = 50;
+
+const INPUT = document.getElementById("imgId");
+
+INPUT.onchange = function (event) {
+  const file = event.target.files[0]; // get the file
+  const blobURL = URL.createObjectURL(file);
+  const img = new Image();
+  img.src = blobURL;
+
+  img.onload = function () {
+    const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
+    const canvas = document.createElement("canvas");
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, newWidth, newHeight);
+    canvas.toBlob((blob) => {
+      // Handle the compressed image.
+      const displayTag = document.createElement("h1");
+      displayTag.innerText = `Original Image - ${readableBytes(
+        file.size
+      )} :::::: Compressed Image - ${readableBytes(blob.size)}`;
+      document.getElementById("container").append(displayTag);
+    });
+  };
+};
+
+function calculateSize(img, maxWidth, maxHeight) {
+  let width = img.width;
+  let height = img.height;
+
+  // calculate the width and height, constraining the proportions
+  if (width > height) {
+    if (width > maxWidth) {
+      height = Math.round((height * maxWidth) / width);
+      width = maxWidth;
+    }
+  } else {
+    if (height > maxHeight) {
+      width = Math.round((width * maxHeight) / height);
+      height = maxHeight;
+    }
+  }
+  return [width, height];
+}
+
+function readableBytes(bytes) {
+  const i = Math.floor(Math.log(bytes) / Math.log(1024)),
+    sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+}
